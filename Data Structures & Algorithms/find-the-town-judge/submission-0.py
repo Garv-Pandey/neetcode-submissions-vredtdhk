d604dd80cc36ecad77f7b12c74c56f64d6d73f48
Garv Pandey <138514913+Garv-Pandey@users.noreply.github.com>
2026-03-30 17:02:16 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        person_arr = [0] *n # index -> person_label, value = incoming trust - outgoing trust
+
+        for i in range(len(trust)):
+            trust_out, trust_in = trust[i]
+
+            person_arr[trust_out - 1] -= 1
+            person_arr[trust_in - 1] += 1
+        
+        print(person_arr)
+
+        for j in range(len(person_arr)):
+            if person_arr[j] == n-1:
+                return j+1
+        
+        return -1
